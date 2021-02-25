eb20ae2659bc13c521941f77d1cf81b94d333954
Luke Bangs <bangsluke@gmail.com>
2021-02-25 18:48:13 +0000

    Initial styling of page.

--- pages/GroupPage/assets/js/jGroupPage.js
+++ pages/GroupPage/assets/js/jGroupPage.js
@@ -2,8 +2,10 @@
 function openNav() {
 	document.getElementById("side-menu").style.width = "300px";
 	document.getElementById("content-area").style.marginRight = "300px";
+    document.getElementById("burgericon").style.display = "none";
 }
 function closeNav() {
 	document.getElementById("side-menu").style.width = "0";
 	document.getElementById("content-area").style.marginRight = "0";
+    document.getElementById("burgericon").style.display = "inline";
 }
