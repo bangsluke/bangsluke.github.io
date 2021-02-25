1d27c7a59d5c97640f93b21c3d464be51411275d
Luke Bangs <bangsluke@gmail.com>
2021-02-25 17:55:34 +0000

    New Group Page file and folder set up.

---
+++ pages/GroupPage/assets/js/jGroupPage.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+	document.getElementById("side-menu").style.width = "300px";
+	document.getElementById("content-area").style.marginRight = "300px";
+}
+function closeNav() {
+	document.getElementById("side-menu").style.width = "0";
+	document.getElementById("content-area").style.marginRight = "0";
+}
