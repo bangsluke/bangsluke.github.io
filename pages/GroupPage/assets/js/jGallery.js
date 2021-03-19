d253a1328764c4f40b7ba95c8e709b4188f80c49
Luke Bangs <bangsluke@gmail.com>
2021-03-19 17:47:34 +0000

    Few additional tweaks to gallery and sub gallery

--- pages/GroupPage/assets/js/jGallery.js
+++ pages/GroupPage/assets/js/jGallery.js
@@ -2,9 +2,9 @@
 
 // Gallery scripts
 
-function passClickedAlbumName() {
+function passClickedAlbumName(album) { // Set the album variable to "this" - see the calling function in the HTML.
     // https://lage.us/Javascript-Pass-Variables-to-Another-Page.html
-    var clickedAlbumName = "General";
+    var clickedAlbumName = album.id; // Get the id of the element that called the function. https://stackoverflow.com/questions/7627161/get-id-of-element-that-called-a-function
     sessionStorage.setItem("clickedAlbumName", clickedAlbumName); // Save the variable to session storage.
     console.log(clickedAlbumName);
 }
