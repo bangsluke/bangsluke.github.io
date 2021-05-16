1ef3eed09db69b0ea4775c5dc0174180e0c5f595
Luke Bangs <bangsluke@gmail.com>
2021-05-16 17:51:42 +0100

    Attempt to add a full screen toggle mode.

---
+++ pages/Group-Page/assets/js/jMainFunctions.js
@@ -0,0 +1,44 @@
+// jMainFunction.js JavaScript Functions
+
+// Main Function scripts used across many pages
+
+// Full Screen functions (https://stackoverflow.com/a/23971798/14290169).
+
+function isFullScreen()
+{
+    return (document.fullScreenElement && document.fullScreenElement !== null)
+         || document.mozFullScreen
+         || document.webkitIsFullScreen;
+}
+
+function requestFullScreen(element)
+{
+    if (element.requestFullscreen)
+        element.requestFullscreen();
+    else if (element.msRequestFullscreen)
+        element.msRequestFullscreen();
+    else if (element.mozRequestFullScreen)
+        element.mozRequestFullScreen();
+    else if (element.webkitRequestFullscreen)
+        element.webkitRequestFullscreen();
+}
+
+function exitFullScreen()
+{
+    if (document.exitFullscreen)
+        document.exitFullscreen();
+    else if (document.msExitFullscreen)
+        document.msExitFullscreen();
+    else if (document.mozCancelFullScreen)
+        document.mozCancelFullScreen();
+    else if (document.webkitExitFullscreen)
+        document.webkitExitFullscreen();
+}
+
+function toggleFullScreen(element)
+{
+    if (isFullScreen())
+        exitFullScreen();
+    else
+        requestFullScreen(element || document.documentElement);
+}
