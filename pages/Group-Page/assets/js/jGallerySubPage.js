e672439ca3ca89cc7603e3743d91b762c2d4ac73
Luke Bangs <bangsluke@gmail.com>
2021-05-19 22:06:20 +0100

    Gallery Sub page updated.

--- pages/Group-Page/assets/js/jGallerySubPage.js
+++ pages/Group-Page/assets/js/jGallerySubPage.js
@@ -20,7 +20,7 @@ function setGalleryHeaderName() {
     if (clickedAlbumName == null) { // Update clickedAlbumName to be General by deafult if error.
         clickedAlbumName = "General";
     }
-    console.log(clickedAlbumName);
+    //console.log(clickedAlbumName);
     document.getElementById("pageMainHeader").innerHTML = clickedAlbumName + " Gallery"; // Modify the text inside the element.
 }
 
@@ -65,16 +65,18 @@ function populateImageSources() {
         default: // Default statement or expression;
             albumSize = 10;
     }
-    console.log(clickedAlbumName + ' album size is ' + albumSize); // Log the album size to be used.
+    //console.log(clickedAlbumName + ' album size is ' + albumSize); // Log the album size to be used.
+    sessionStorage.setItem("albumSize", albumSize); // Save the variable to session storage.
     var x; // Define the variable for looping.
     for (x = 1; x <= maxImageCount; x++) { // Loop through all images.
-        console.log(x);
+        //console.log(x);
         if (x <= albumSize) { // If the image number falls within the album size, update the source of the image.
             var sourcePath = "/pages/Group-Page/assets/images/Gallery/" + clickedAlbumName + "/" + clickedAlbumName + x + ".jpg"; // Define the source path used for both images.
             document.getElementById("image" + x).src = sourcePath; // Set the source of the image dynamically.
             document.getElementById("modalimage" + x).src = sourcePath; // Set the source of the modal image dynamically.
         } else { // If the image number is larger than the album size, hide the placeholder.
             document.getElementById("image" + x).classList.add("hidden"); // Add the hidden class to the image.
+            document.getElementById("modalimage" + x).classList.add("hidden"); // Add the hidden class to the modal image.
         }
     }
 }
@@ -96,6 +98,9 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
+    console.log("plusSlides called and passed n = " + n);
+    console.log("slideIndex was = " + slideIndex);
+    console.log("slideIndex is = " + (slideIndex += n));
     showSlides(slideIndex += n); // Call the show slides function, passing the next or previous integer.
 }
 
@@ -105,25 +110,61 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i; // Define the variable for looping.
+    var clickedAlbumName = sessionStorage.getItem("clickedAlbumName", clickedAlbumName); // Get the saved variable from session storage.
+    var albumSize = sessionStorage.getItem("albumSize", albumSize); // Get the saved variable from session storage.
+    if (clickedAlbumName == null) { // Update clickedAlbumName to be General by deafult if error.
+        clickedAlbumName = "General";
+    }
     var slides = document.getElementsByClassName("mySlides"); // Get all images with class "mySlides".
+    console.log(slides.length);
     //var dots = document.getElementsByClassName("demo"); // Get all images with class "dots".
     //var captionText = document.getElementById("caption"); // Get the caption id text.
-    if (n > slides.length) { // Deal with n going over the maximum number of images.
-        slideIndex = 1
+    // if (n == 0) {
+    //     slideIndex = albumSize;
+    // }
+    if (n > albumSize) { // Deal with n going over the maximum number of images.
+    //if (n > slides.length) { // Deal with n going over the maximum number of images.
+        slideIndex = 1;
     }
     if (n < 1) { // Deal with n going under 1.
-        slideIndex = slides.length
+        console.log("Before change n = " + n);
+        //slideIndex = slides.length;
+        slideIndex = albumSize;
+        console.log("After change slideIndex = " + slideIndex);
     }
+    var i; // Define the variable for looping.
     // Update the image number text.
-    document.getElementById("numbertext").innerHTML = slideIndex + "/" + slides.length; // Modify the text inside the element.
-    for (i = 0; i < slides.length; i++) {
+    document.getElementById("numbertext").innerHTML = slideIndex + "/" + albumSize; // Modify the text inside the element.
+    //document.getElementById("numbertext").innerHTML = slideIndex + "/" + slides.length; // Modify the text inside the element.
+    for (i = 0; i < albumSize; i++) {
+    //for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     //for (i = 0; i < dots.length; i++) { // Loop through the images with class "dots".
     //dots[i].className = dots[i].className.replace(" active", "");
     //}
-    slides[slideIndex - 1].style.display = "block";
+
+    var x = n; // Set up the x variable.
+    //console.log("x = " + x);
+    var img = new Image(); // Get the image height and width from making a new image. https://stackoverflow.com/a/5633302/14290169.
+    img.src = "/pages/Group-Page/assets/images/Gallery/" + clickedAlbumName + "/" + clickedAlbumName + x + ".jpg"; // Setting the source.
+    var height = img.height // Getting the height.
+    var width = img.width // getting the width.
+    console.log("image height is = " + height + " and width is " + width);
+
+    if (height >= width ) {
+        // Image type is portrait-image.
+        document.getElementById("modalimage" + x).classList.add("portrait-image"); // Add the portrait-image class to the div housing the image.
+        console.log("Height is bigger than width so applying portrait class.")
+    } else {
+        // Image type is landscape-image.
+        document.getElementById("modalimage" + x).classList.add("landscape-image"); // Add the landscape-image class to the div housing the image.
+        console.log("Width is bigger than height so applying landscape class.")
+    }
+ 
+    console.log(n);
+    slides[slideIndex - 1].style.display = "block"; // Load the selected image.
+    
     // dots[slideIndex - 1].className += " active"; // Add the active class to the demo class image.
     //captionText.innerHTML = dots[slideIndex - 1].alt; // Update the caption text based on the alt property of the demo class image.
 }
