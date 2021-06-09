6f2aa965dd9eefcfdb964a5c798f58984be41cf8
Luke Bangs <bangsluke@gmail.com>
2021-06-09 18:16:00 +0100

    Additional snap tabs fixes

---
+++ pages/Group-Page/assets/components/LoaderDots/LoaderDots.js
@@ -0,0 +1,5 @@
+// Hide the loader dots when all images loaded.
+function hideLoaderDots() {
+    console.log("Loader dots hidden.");
+    document.getElementsByClassName("loader").classList.add("hidden"); // Add the hidden class to the loader-dots-container.
+}
