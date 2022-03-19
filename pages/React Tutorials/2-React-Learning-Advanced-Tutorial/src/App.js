19f112eb25dda46123d791cb9204b57a8ed65240
Luke Bangs <bangsluke@gmail.com>
2022-03-19 18:44:55 +0000

    Final clean up of files, structure and VSCode extensions

--- pages/React Tutorials/2-React-Learning-Advanced-Tutorial/src/App.js
+++ pages/React Tutorials/2-React-Learning-Advanced-Tutorial/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+
 function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+    return (
+        <div className="container" >
+            <h2> 2 React Advanced Tutorial </h2>{" "}
+            <h3><a href="https://www.youtube.com/watch?v=4UZrsTqkcW4&t=12175s&ab_channel=freeCodeCamp.org">Link here</a> @3:33:00</h3>
+        </div>
+    );
 }
 
-export default App
+export default App;
